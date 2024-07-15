c9ecdaf2690b6ab1336537e9fa7edc7ee96df6ee
Elliot Coiper <elliotcoiper@gmail.com>
2024-07-15 15:24:48 +0400

    Added https

--- src/useMovies.tsx
+++ src/useMovies.tsx
@@ -29,7 +29,7 @@ export function useMovies(query: string) {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
           if (!res.ok)
