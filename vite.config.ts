9da68633bad4108069e78c07a5dc84cce9b534cd
Elliot Coiper <elliotcoiper@gmail.com>
2024-07-15 17:59:01 +0400

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/MovieSearcher/",
 });
-=======
-  base: "/MovieSearcher/", 
-})
->>>>>>> fd1c69add40c004f9e9574f7d9875dd609c523cd
